1de2444477822c1c29288d50eaf721cd7a86ce71
Sergio Dzul <Sergio Dzul@SDZUL-IMASD-DES>
2025-03-20 13:07:04 -0600

    Tickets de transporte y consultas de ayuda

--- OracleScripts.sql
+++ OracleScripts.sql
@@ -1,5 +1,16 @@
+Select * from APP.usuario where SCORREO = 'karly_1992_@hotmail.com'
 
 
-SELECT * FROM
+
+
+Select * from APP.usuario where SCORREO = 'karly_1992_@hotmail.com'
+
+
+
+
+
+SELECT * FROM APPMONEDEROQUERY.TARJETAUSUARIO WHERE UIDUSUARIO = (SELECT UIDUSUARIO FROM APP.USUARIO WHERE SCORREO = 'sebassdc@hotmail.com')
+
+
 
 
